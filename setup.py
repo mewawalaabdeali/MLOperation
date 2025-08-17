526a302f1533e418d5de64afb4d3a335f9940d1f
ali <ali_abde37@outlook.com>
2025-08-17 12:58:41 -0400

    initial commmit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='END to END ML Operation with DVC, DAGSHUB, MLFLOW',
+    author='AbdeALI',
+    license='MIT',
+)
